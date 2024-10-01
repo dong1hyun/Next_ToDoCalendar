f5bcb05a63d93437731ed37fb47f57a5bc1ae414
dong1hyun <limd1238@naver.com>
2024-10-01 12:50:12 +0900

    test

--- app/(main)/home/[...date]/page.tsx
+++ app/(main)/home/[...date]/page.tsx
@@ -12,117 +12,128 @@ interface urlForm {
     }
 }
 
-// export const findUser = async () => {
-//     const session = await getSession();
-//     const data = await getServerSession();
-//     const google_email = data?.user?.email;
-//     const exist_info: {id?: number; email?: string} = {};
-//     if(session.id) exist_info.id = session.id;
-//     if(google_email) exist_info.email = google_email;
-//     return exist_info;
-// }
+export const findUser = async () => {
+    const session = await getSession();
+    const data = await getServerSession();
+    const google_email = data?.user?.email;
+    const exist_info: {id?: number; email?: string} = {};
+    if(session.id) exist_info.id = session.id;
+    if(google_email) exist_info.email = google_email;
+    return exist_info;
+}
 
-// const getToDoCount = async (year: number, month: number, day: number, user: any) => {
-//     const count = await db.toDo.count({
-//         where: {
-//             year,
-//             month,
-//             day,
-//             isComplete: false,
-//             user
-//         }
-//     });
-//     return count;
-// }
+const getToDoCount = async (year: number, month: number, day: number, user: any) => {
+    const count = await db.toDo.count({
+        where: {
+            year,
+            month,
+            day,
+            isComplete: false,
+            user
+        }
+    });
+    return count;
+}
 
-// const getCompleteCount = async (year: number, month: number, day: number, user: any) => {
-//     const count = await db.toDo.count({
-//         where: {
-//             year,
-//             month,
-//             day,
-//             isComplete: true,
-//             user
-//         }
-//     });
-//     return count;
-// }
+const getCompleteCount = async (year: number, month: number, day: number, user: any) => {
+    const count = await db.toDo.count({
+        where: {
+            year,
+            month,
+            day,
+            isComplete: true,
+            user
+        }
+    });
+    return count;
+}
 
-// export const getTypeCount = async (type: string, year: number, month: number ,isComplete: boolean) => {
-//     const user = await findUser();
-//     const count = await db.toDo.count({
-//         where: {
-//             type,
-//             year,
-//             month,
-//             isComplete,
-//             user
-//         }
-//     });
-//     return count;
-// }
+export const getTypeCount = async (type: string, year: number, month: number ,isComplete: boolean) => {
+    const user = await findUser();
+    const count = await db.toDo.count({
+        where: {
+            type,
+            year,
+            month,
+            isComplete,
+            user
+        }
+    });
+    return count;
+}
 
 export default async function Home({ params }: urlForm) {
     // await new Promise((resolve) => setTimeout(resolve, 1000));
-    // const year = +params.date[0];
-    // const month = +params.date[1];
-    // const limit = new Date(year, month, 0).getDate();
-    // const toDoCount: number[] = [];
-    // const completeCount: number[] = [];
-    // const user = await findUser();
-    // console.log("월, 달", year, month);
-    // try {
-    //     for (let i = 1; i <= limit; i++) {
-    //         console.log(i);
-    //         const count = await getToDoCount(year, month, i, user);
-    //         toDoCount.push(count);
-    //         const count2 = await getCompleteCount(year, month, i, user);
-    //         completeCount.push(count2);
-    //     }
-    // } catch (error) {
-    //     console.error("count 에러:", error);
-    // }
-    // console.log("count", toDoCount, completeCount);
-    // const work = await getTypeCount("업무", year, month, false);
-    // const friend = await getTypeCount("지인", year, month, false);
-    // const individual = await getTypeCount("개인", year, month, false);
-    // const education = await getTypeCount("교육", year, month, false);
-    // const social = await getTypeCount("사회활동", year, month, false);
-    // const data = [
-    //     {
-    //         "id": "업무",
-    //         "label": "업무",
-    //         "value": work,
-    //     },
-    //     {
-    //         "id": "지인",
-    //         "label": "지인",
-    //         "value": friend,
-    //     },
-    //     {
-    //         "id": "개인",
-    //         "label": "개인",
-    //         "value": individual,
-    //     },
-    //     {
-    //         "id": "교육",
-    //         "label": "교육",
-    //         "value": education,
-    //     },
-    //     {
-    //         "id": "사회활동",
-    //         "label": "사회활동",
-    //         "value": social,
-    //     },
-    // ]
+    const year = +params.date[0];
+    const month = +params.date[1];
+    const limit = new Date(year, month, 0).getDate();
+    const toDoCount: number[] = [
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0
+      ];
+    const completeCount: number[] = [
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0
+      ];
+    const user = await findUser();
+    console.log("월, 달", year, month);
+    
+    try {
+        // for (let i = 1; i <= limit; i++) {
+        //     console.log(i);
+        //     const count = await getToDoCount(year, month, i, user);
+        //     toDoCount.push(count);
+        //     const count2 = await getCompleteCount(year, month, i, user);
+        //     completeCount.push(count2);
+        // }
+    } catch (error) {
+        console.error("count 에러:", error);
+    }
+    console.log("ㅊㅊㅊㅊㅊㅊ")
+    console.log("count", toDoCount, completeCount);
+    const work = await getTypeCount("업무", year, month, false);
+    const friend = await getTypeCount("지인", year, month, false);
+    const individual = await getTypeCount("개인", year, month, false);
+    const education = await getTypeCount("교육", year, month, false);
+    const social = await getTypeCount("사회활동", year, month, false);
+    const data = [
+        {
+            "id": "업무",
+            "label": "업무",
+            "value": work,
+        },
+        {
+            "id": "지인",
+            "label": "지인",
+            "value": friend,
+        },
+        {
+            "id": "개인",
+            "label": "개인",
+            "value": individual,
+        },
+        {
+            "id": "교육",
+            "label": "교육",
+            "value": education,
+        },
+        {
+            "id": "사회활동",
+            "label": "사회활동",
+            "value": social,
+        },
+    ]
     return (
         <div className="h-screen flex flex-col xl:flex-row justify-center items-center pt-24 xl:pt-0">
-            {/* <Calendar toDoCount={toDoCount} completeCount={completeCount} /> */}
+            <Calendar toDoCount={toDoCount} completeCount={completeCount} />
             <div className="flex flex-col items-center mt-6">
                 <h1 className="text-xl font-bold">당월 할 일 차트</h1>
                 <div className="w-80 h-80">
-                    달력
-                    {/* <MyResponsivePie data={data} colors={['#c56cf0', '#706fd3', '#34ace0', '#ff793f', '#e74c3c']} /> */}
+                    <MyResponsivePie data={data} colors={['#c56cf0', '#706fd3', '#34ace0', '#ff793f', '#e74c3c']} />
                 </div>
             </div>
         </div>
