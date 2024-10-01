4b3f72e9ec9aefd58fa6e941ad3e9edf69b1feed
dong1hyun <limd1238@naver.com>
2024-10-01 15:09:38 +0900

    test

--- app/(main)/home/[...date]/page.tsx
+++ app/(main)/home/[...date]/page.tsx
@@ -102,45 +102,45 @@ export default async function Home({ params }: urlForm) {
     } catch (error) {
         console.error("count 에러:", error);
     }
-    const work = await getTypeCount("업무", year, month, false);
-    const friend = await getTypeCount("지인", year, month, false);
-    const individual = await getTypeCount("개인", year, month, false);
-    const education = await getTypeCount("교육", year, month, false);
-    const social = await getTypeCount("사회활동", year, month, false);
-    const data = [
-        {
-            "id": "업무",
-            "label": "업무",
-            "value": work,
-        },
-        {
-            "id": "지인",
-            "label": "지인",
-            "value": friend,
-        },
-        {
-            "id": "개인",
-            "label": "개인",
-            "value": individual,
-        },
-        {
-            "id": "교육",
-            "label": "교육",
-            "value": education,
-        },
-        {
-            "id": "사회활동",
-            "label": "사회활동",
-            "value": social,
-        },
-    ]
+    // const work = await getTypeCount("업무", year, month, false);
+    // const friend = await getTypeCount("지인", year, month, false);
+    // const individual = await getTypeCount("개인", year, month, false);
+    // const education = await getTypeCount("교육", year, month, false);
+    // const social = await getTypeCount("사회활동", year, month, false);
+    // const data = [
+    //     {
+    //         "id": "업무",
+    //         "label": "업무",
+    //         "value": work,
+    //     },
+    //     {
+    //         "id": "지인",
+    //         "label": "지인",
+    //         "value": friend,
+    //     },
+    //     {
+    //         "id": "개인",
+    //         "label": "개인",
+    //         "value": individual,
+    //     },
+    //     {
+    //         "id": "교육",
+    //         "label": "교육",
+    //         "value": education,
+    //     },
+    //     {
+    //         "id": "사회활동",
+    //         "label": "사회활동",
+    //         "value": social,
+    //     },
+    // ]
     return (
         <div className="h-screen flex flex-col xl:flex-row justify-center items-center pt-24 xl:pt-0">
             <Calendar toDoCount={toDoCount} completeCount={completeCount} />
             <div className="flex flex-col items-center mt-6">
                 <h1 className="text-xl font-bold">당월 할 일 차트</h1>
                 <div className="w-80 h-80">
-                    <MyResponsivePie data={data} colors={['#c56cf0', '#706fd3', '#34ace0', '#ff793f', '#e74c3c']} />
+                    {/* <MyResponsivePie data={data} colors={['#c56cf0', '#706fd3', '#34ace0', '#ff793f', '#e74c3c']} /> */}
                 </div>
             </div>
         </div>
