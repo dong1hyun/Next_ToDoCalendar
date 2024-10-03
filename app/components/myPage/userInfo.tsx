75f1166d1c3199c80fee86d7ddf7473bb96ae506
dong1hyun <limd1238@naver.com>
2024-10-03 22:08:42 +0900

    헤더, 버튼 css 수정

--- app/components/myPage/userInfo.tsx
+++ app/components/myPage/userInfo.tsx
@@ -15,7 +15,7 @@ export default function UserInfo({user}: {user:userForm}) {
         <div className="flex flex-col items-start gap-4 mt-32">
             <div>이름: {user.username}</div>
             <div>이메일: {user.email}</div>
-            {isGoogleLogin.data ? <button onClick={() => signOut({ callbackUrl: "/" })} >로그아웃</button>
+            {isGoogleLogin.data ? <button className={`${buttonStyle} bg-black px-4 py-2`} onClick={() => signOut({ callbackUrl: "/" })} >로그아웃</button>
                 : <form action={logOut}>
                     <button className={`${buttonStyle} bg-black px-4 py-2`}>로그아웃</button>
                 </form>}
